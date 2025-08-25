7908189d0144f6ef19f02b835774a5a3b77f8a61
mahosinislam <mahosinislam80@gmail.com>
2025-08-24 23:18:30 -0700

    modify some items

--- Home.js
+++ Home.js
@@ -5,12 +5,14 @@ const digitPin=document.getElementById('disit-pin');
 
 const loginBtn=document.getElementById('Login-btn')
 
-loginBtn.addEventListener('click',function(){
+const man=23123456789;
 
+const pin=9753;
+loginBtn.addEventListener('click',function(){
  let mobile= mobileNumbr.value;
 let digit=digitPin.value;
-     if(mobile.length == 11) {
-        if(digit.length==4){
+     if(mobile == man) {
+        if(digit==pin){
                     window.location.href='./stor-html/Payo.html'
         }
         else{
--- stor-js/addmoney.js
+++ stor-js/addmoney.js
@@ -1,21 +1,55 @@
+function setValue(id){
+   const home= document.getElementById(id).value;
+   const maho=parseInt(home);
+   return maho;
+}
+//inner text valu function
+function inne(id){
+   const man =document.getElementById(id).innerText;
+   const mae=parseInt(man);
+   return mae;
+}
+
 document.getElementById('addmoney-btn').addEventListener('click',function(e){
     e.preventDefault();
-    const check=document.getElementById('btn-type').value;
-    const  totalAmount=parseInt(document.getElementById('total').innerText);
+    //curent amount select
+   //  const  totalAmount=parseInt(document.getElementById('total').innerText);
+   const totalAmount=inne('total');
+    //bank seleck
     const bank=document.getElementById('select-bank').innerText;
-    const addMoney=parseInt(document.getElementById('addamount').value);
-    const pinNumber=parseInt(document.getElementById('pin-number').value);
-    const pin=document.getElementById('pin-number').value;
+    //amount acount number
+    const banckAccount=document.getElementById('bank-account').value;
+    const bankLenght=banckAccount.length;
+    //select amount
+   const addMoney=setValue('addamount')
+   
+    //select pin 
+    const pinNumber=document.getElementById('pin-number').value;
+    const pinLenth=pinNumber.length;
 
-    console.log(check);
+//condition 
+if( bankLenght!=11){
+   alert('invalid number');
+   return;
+}
+if(pinLenth!=4){
+    alert('invalid pin');
+   return;
+}
+    //sum
     const resutl =totalAmount + addMoney;
    document.getElementById('total').innerText=resutl;
 
 });
 document.getElementById('baba').addEventListener('click', function(){
-   const totalAmount2=parseInt(document.getElementById('total').innerText); 
-  const withDrow=parseInt(document.getElementById('with-drow').value);
-
+   const withDrow=setValue('with-drow');
+   const totalAmount2 =inne('total')
+   const pinWid=document.getElementById('pine').value;
+   const pinLen=pinWid.length;
+   if(pinLen!=4){
+      alert('wrong your pin');
+      return;
+   }
   const valid=totalAmount2 - withDrow;
   document.getElementById('total').innerText=valid;
   
